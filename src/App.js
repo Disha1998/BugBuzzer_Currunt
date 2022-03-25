85f0f336a765c2f6e21a6ba546b9066f8354424f
Twinkalsoni <twinkal211@gmail.com>
2022-03-25 18:04:40 +0530

    completed detail page

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Container } from '@mui/material';
+import Navbar from './components/Navbar';
+import Intro from './components/Intro';
+import Bugs from './components/Bugs';
+import Compaigns from './components/Compaigns';
+import QandAList from './components/QandAList';
+import BountyWinners from './components/BountyWinners';
+import Footer from './components/Footer';
+// import DetailBounties from './components/DetailBounties';
+import DetailQandA from './components/DetailQandA';
+import {Routes,Route,}from "react-router-dom";
+import DashBoard from './Dashboard'
+import DashboardNav from './components/DashboardNav';
+import Detailbutton from './components/Detailbutton';
+
+const App = () => {
+  return (
+    <>
+
+ {/* <Navbar /> */}
+
+{/* <Intro />
+<Bugs />
+<Compaigns />
+<QandAList />
+<BountyWinners /> */}
+<Container>
+<DashboardNav/>
+  <Routes>
+          <Route path="Q&A/*" element={<DetailQandA />}/>
+          <Route path="Compaigns/*" element={<Compaigns/>}/>
+         <Route path="Detailbutton/*" element={<Detailbutton />} />
+
+          <Route path="/" element={  <DashBoard />}/>
+           {/* <Route path="/bounty" element={<DetailBounties />} /> */}
+        </Routes>
+        </Container>
+        <Footer />  
+       
+
+ </>
+    )
+}
+
+export default App
+
+  
---
+++ src/components/Detailbutton.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Box } from '@mui/system'
+import Avatar from '@mui/material/Avatar';
+
+import { Container, Stack, Typography } from '@mui/material'
+
+const Detailbutton = () => {
+    return (
+        <>
+            <Container>
+                <Stack direction="row" mt={20} mb={19} flexWrap='wrap'>
+
+                    <Box sx={{ height: '193px', width: '473px', border: '1px solid black', background: '#FFFFFF', p: 1, m: 1, }}>
+                        <Typography variant='h6' align='center'> Detail</Typography>
+                    </Box>
+                    <Box sx={{ height: '193px', mt: "5", width: '473px', border: '1px solid black', background: '#FFFFFF', p: 1, m: 1, }}>
+                        <Typography variant='subtitle1' align='center'> Organized by</Typography>
+                        <Stack direction="row" spacing={2}>
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 100, height: 100, ml: "40%" }} />
+                        </Stack>
+                        <Typography variant='subtitle1' align='center' mt={3}> Name</Typography>
+
+                    </Box>
+                </Stack>
+
+            </Container>
+        </>
+    )
+}
+
+export default Detailbutton
